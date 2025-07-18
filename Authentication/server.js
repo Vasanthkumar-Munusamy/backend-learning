5c0943c71dff3383ba32a62561ded6638a50a953
Vasanthkumar-chargeMOD12 <vasanth.kumar@chargemod.com>
2025-07-18 14:19:24 +0530

    implemented server is running on the port 8000. And installed express, nodemon, dotenv to run the server.

---
+++ Authentication/server.js
@@ -0,0 +1,8 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+    console.log("Server is started on the port", PORT);
+})
