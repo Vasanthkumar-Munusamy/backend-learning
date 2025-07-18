6c3d7beaa34a711c5373276850c6b1244a525b84
Vasanthkumar-chargeMOD12 <vasanth.kumar@chargemod.com>
2025-07-18 14:32:46 +0530

    Handled Undefined routes and test route.

--- Authentication/server.js
+++ Authentication/server.js
@@ -3,6 +3,13 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT;
 
+app.get("/test", (req, res) => {
+    res.status(200).json({ status_code: 200, message: "Test is successfull." });
+})
+app.use((req, res) => {
+    res.status(404).json({ status_code: 404, message: "Page Not Found." });
+});
+
 app.listen(PORT, () => {
     console.log("Server is started on the port", PORT);
 })
